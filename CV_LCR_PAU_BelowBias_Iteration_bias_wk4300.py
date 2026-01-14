3cd0f0d750df05fc9cbff03bd6df19fde66297ff
Young Park <bnmhjk13@gmail.com>
2026-01-14 16:24:06 +0900

    updated CV measurement code

--- CV_LCR_PAU_BelowBias_Iteration_bias_wk4300.py
+++ CV_LCR_PAU_BelowBias_Iteration_bias_wk4300.py
@@ -1,28 +1,37 @@
-# test LCR meter
 import os
 import numpy as np
-import pylab as plt
+import matplotlib.pyplot as plt
 import pyvisa
 import time
 import signal
 import matplotlib as mp
-from util import mkdir, getdate
+from datetime import datetime
+from pathlib import Path
 
-mp.rcParams.update({'font.size':15})
+# --- Setup Plotting Style ---
+mp.rcParams.update({'font.size': 15})
 
+# --- Configuration & Paths ---
 opathroot = r'C:\LGAD_test\C-V_test' 
-#sensorname = r'FBK_USFD3_CMS_2'
-#sensorname = r'code test'
 sensorname = r'UFSD-K2_2x2'
 Nmeas = r'test20250731'
 Npad = '1_1'
 Ntimes = True
-Iteration = int(input("Enter a positive integer for Iteration: "))
 
+# --- Utility Functions (Replacing 'util' library) ---
+def getdate():
+    return datetime.now().strftime("%Y%m%d")
+
+def mkdir(path):
+    Path(path).mkdir(parents=True, exist_ok=True)
+
+# --- Measurement parameters ---
+Iteration = int(input("Enter a positive integer for Iteration (Averaging): "))
 V0 = 0 
 V1 = -40
 npts = 81
 
+# Optional fine-sweep range
 V2 = -15
 V3 = -25
 npts1 = 51
@@ -32,51 +41,56 @@ date = getdate()
 def CVmeasurement(freq, return_sweep=False):
     rm = pyvisa.ResourceManager()
     rlist = rm.list_resources()
-    print (rlist)
+    print("Resources found:", rlist)
 
-    pau = rm.open_resource('GPIB0::22::INSTR')
-    lcr = rm.open_resource('GPIB0::6::INSTR')
-    lcr.read_termination = '\n'
-    lcr.write_termination = '\n'
+    # Initialize Instruments
+    try:
+        pau = rm.open_resource('GPIB0::22::INSTR')
+        lcr = rm.open_resource('GPIB0::6::INSTR')
+        lcr.read_termination = '\n'
+        lcr.write_termination = '\n'
+        pau.read_termination = '\n' # Added for stability
+    except Exception as e:
+        print(f"Error connecting to instruments: {e}")
+        return
 
-    print(pau.query("*IDN?"))
-    print(lcr.query('*IDN?'))
+    print("PAU IDN:", pau.query("*IDN?"))
+    print("LCR IDN:", lcr.query("*IDN?"))
 
+    # Configure Picoammeter (PAU)
     pau.write("*RST")
     pau.write("curr:range 2e-4")
     pau.write("INIT")
     pau.write("syst:zch off")
     pau.write("SOUR:VOLT:STAT off")
     pau.write("SOUR:VOLT:RANG 500")
-    #pau.write("SOUR:VOLT:ILIM 2.5e-5")
     pau.write("SOUR:VOLT:ILIM 1e-4")
     pau.write("FORM:ELEM READ,UNIT,STAT,VSO")
 
+    # Configure LCR Meter
     lcr.write(":MEAS:NUM-OF-TESTS 1")
     lcr.write(":MEAS:FUNC1 C")
     lcr.write(":MEAS:FUNC2 Z")
     lcr.write(":meas:equ-cct par")
-    lcr.write(":MEAS:SPEED fast")
+    lcr.write(":MEAS:SPEED med") # Changed from fast to med for better SNR on LGADs
     lcr.write(":MEAS:LEV 0.1")
     lcr.write(":MEAS:V-BIAS 0V")
     lcr.write(f":MEAS:FREQ {freq}")
 
+    # Interrupt Handler for Safety
     def handler(signum, frame):
-        print ("User interrupt. Turning off the output ...")
+        print("\nUser interrupt! Safely ramping down...")
         pau.write(":sour:volt:lev 0")
         pau.write(":sour:volt:stat off")
         lcr.write(":MEAS:BIAS OFF")
-        lcr.write(":MEAS:V-BIAS 0V")
         lcr.close()
-        print ("WARNING: Please make sure the output is turned off!")
-
+        pau.close()
         exit(1)
 
     signal.signal(signal.SIGINT, handler)
 
-    ## C-V
+    # --- Generate Voltage Array ---
     Varr = np.linspace(V0, V1, npts)
-
     if (V2 is not None):
         if (V2 > V1) and (V3 > V1):
             VarrL = Varr[Varr > V2]
@@ -84,131 +98,122 @@ def CVmeasurement(freq, return_sweep=False):
             VarrM = np.linspace(V2, V3, npts1)
             Varr = np.concatenate([VarrL, VarrM, VarrH])
 
-    print (Varr)
-
     if return_sweep:
-        Varr_return = np.linspace(V1,V0,int(abs(V0-V1)/10 + 1))
+        Varr_return = np.linspace(V1, V0, int(abs(V0-V1)/10 + 1))
         Varr = np.concatenate([Varr, Varr_return])
 
-    Vpau_arr = []
-    Ipau_arr = []
-    CV_arr = []
-    RV_arr = []
+    # --- Measurement Loop ---
+    Vpau_arr, Ipau_arr, CV_arr, RV_arr = [], [], [], []
 
     pau.write(":sour:volt 0")
     pau.write(":sour:volt:stat on")
-    lcr.write(":MEAS:V-BIAS 0V")
     lcr.write("meas:bias OFF")
     time.sleep(1)
 
+    print("\nStarting Sweep (V_target | V_actual | I_pau | Cap | Res)")
     t0 = time.time()
+    
     for Vdc in Varr:
         if Vdc > 0:
-            print ("Warning: positive bias is not allowed. Setting DC voltage to 0.")
-            Vdc = 0
+            Vdc = 0 # Safety: no forward bias
 
         pau.write(f':sour:volt {Vdc}')
-        time.sleep(0.01)
-        Ipau, stat_pau, Vpau = pau.query(":READ?").split(',')
-
-        Vpau = float(Vpau)
-        Ipau = float(Ipau[:-1])
-        Vpau_arr.append(Vdc)
-        Ipau_arr.append(Ipau)
-
-        if Ntimes and Iteration > 0:
-            capacitance_values = []
-            r_values = []
-
-            for _ in range(Iteration):
-                res = lcr.query('meas:trig?')
-                C0, R0 = res.split(',')
-                try:
-                    C0 = float(C0)
-                    R0 = float(R0)
-                except:
-                    break
-
-                capacitance_values.append(C0)
-                r_values.append(R0)
-                time.sleep(0.05)  # Pauses for N seconds between iterations        
-
-            Cavg = np.mean(capacitance_values)
-            Ravg = np.mean(r_values)
-            CV_arr.append(Cavg)
-            RV_arr.append(Ravg)
+        time.sleep(0.05) 
         
-        else:
-            print("Invalid input.")
+        # Read Picoammeter
+        raw_pau = pau.query(":READ?").split(',')
+        Ipau = float(raw_pau[0])
+        Vpau = float(raw_pau[3])
 
-        print(round(Vdc, 1), Vpau, Ipau, C0, R0)
+        # Read LCR Meter with Averaging
+        cap_samples = []
+        res_samples = []
+        
+        for _ in range(max(1, Iteration)):
+            res = lcr.query('meas:trig?')
+            try:
+                c_val, r_val = map(float, res.split(','))
+                cap_samples.append(c_val)
+                res_samples.append(r_val)
+            except ValueError:
+                continue
+            time.sleep(0.02)
+
+        Cavg = np.mean(cap_samples) if cap_samples else 0
+        Ravg = np.mean(res_samples) if res_samples else 0
+
+        Vpau_arr.append(Vpau)
+        Ipau_arr.append(Ipau)
+        CV_arr.append(Cavg)
+        RV_arr.append(Ravg)
+
+        print(f"{Vdc:.1f}V | {Vpau:.2f}V | {Ipau:.2e}A | {Cavg:.3e}F | {Ravg:.2e}R")
 
     t1 = time.time()
-    print(f"* Bias sweep of {npts} samples between {V0} and {V1}")
-    print(f"   * Return sweep: {return_sweep}")
-    print(f"   * Elapsed time = {t1-t0} s")
+    
+    # --- Shutdown ---
     pau.write(":sour:volt 0")
     pau.write(":sour:volt:stat off")
     pau.close()
     lcr.write(":MEAS:BIAS OFF")
-    lcr.write(":MEAS:V-BIAS 0V")
     lcr.close()
 
+    # --- Save Data ---
     opath = os.path.join(opathroot, Nmeas, f"{date}_{sensorname}")
     mkdir(opath)
-    fname = f'CV_LCR+PAU_{sensorname}_{date}_{freq}Hz_pad{Npad}'
-    ofname = os.path.join(opath, fname)
+    
+    base_fname = f'CV_LCR+PAU_{sensorname}_{date}_{freq}Hz_pad{Npad}'
+    ofname = os.path.join(opath, base_fname)
 
     i = 0
-    while os.path.isfile(ofname+'.txt'):
-        fname = f'CV_LCR+PAU_{sensorname}_{date}_{freq}Hz_pad{Npad}_{i}'
-        ofname = os.path.join(opath, fname)
+    while os.path.isfile(ofname + '.txt'):
+        ofname = os.path.join(opath, f"{base_fname}_{i}")
         i += 1
 
     header = 'Vpau(V)\tC(F)\tR(Ohm)\tIpau(A)'
+    data_to_save = np.array([Vpau_arr, CV_arr, RV_arr, Ipau_arr]).T
+    np.savetxt(ofname + '.txt', data_to_save, header=header, delimiter='\t')
+    
+    print(f"\nMeasurement complete. Elapsed: {t1-t0:.1f}s")
+    print(f"Data saved to: {ofname}.txt")
 
-    np.savetxt(ofname+'.txt', np.array([Vpau_arr, CV_arr, RV_arr, Ipau_arr]).T, header=header)
-    plot_cv(ofname+'.txt', freq) 
-    plt.savefig(ofname+'.png')
-
+    plot_cv(ofname + '.txt', freq)
+    plt.savefig(ofname + '.png')
 
 def plot_cv(fname, freq=None):
-    try:
-        V, C, R = np.genfromtxt(fname).T
-    except:
-        V, C, R, I = np.genfromtxt(fname).T
-    fig, ax1 = plt.subplots()
+    # Load data, skipping header
+    data = np.genfromtxt(fname)
+    V = data[:, 0]
+    C = data[:, 1]
+    R = data[:, 2]
 
-    if (V[1] < 0):
-        V = -1 * V
+    # Use absolute value of Voltage for standard C-V plotting
+    V_plot = np.abs(V)
     
-    ax1.plot(V, C*1e9, 'x-', color='tab:blue', markersize=5, linewidth=0.5, label="$C$")
-    ax1.set_xlabel('Bias (V)')
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+
+    # Axis 1: Capacitance (nF)
+    ax1.plot(V_plot, C * 1e9, 'o-', color='tab:blue', label="Capacitance")
+    ax1.set_xlabel('Bias Voltage |V| (V)')
     ax1.set_ylabel('C (nF)', color='tab:blue')
+    ax1.tick_params(axis='y', labelcolor='tab:blue')
+    ax1.grid(True, alpha=0.3)
+
+    # Axis 2: 1/C^2 (Standard for Doping/Depletion)
     ax2 = ax1.twinx()
-    ax2.plot(V, R, 'x-', color='tab:red')
-    ax2.set_ylabel('R (Ohm)', color='tab:red')
-    #ax2.set_yscale('log')
-    ax3 = ax1.twinx()
-    ax3.plot(V, 1/(C)**2, 'x-', color='tab:green', markersize=5, linewidth = 0.5, label="$1/C^2$")
-    ax3.set_ylabel('$1/C^2 ($F$^{-2})$', color='tab:green')
-    #ax3.set_yscale('log')
+    inv_c2 = 1.0 / (C**2)
+    ax2.plot(V_plot, inv_c2, 's-', color='tab:green', alpha=0.6, label="$1/C^2$")
+    ax2.set_ylabel('$1/C^2$ ($F^{-2}$)', color='tab:green')
+    ax2.tick_params(axis='y', labelcolor='tab:green')
 
+    plt.title(f"C-V Test: {sensorname} ({freq} Hz)")
     fig.tight_layout()
-
 
 def cvtest():
     freq = int(1e3)
-    print("frequency [Hz] :", freq)
-
-    return_sweep = True
-    CVmeasurement(freq, return_sweep)
-    #CVmeasurement(100, return_sweep)
+    print("Frequency [Hz] :", freq)
+    CVmeasurement(freq, return_sweep=True)
     plt.show()
-    return 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     cvtest()
-
-    plt.show()
